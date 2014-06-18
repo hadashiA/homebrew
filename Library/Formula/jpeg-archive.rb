431b4349a5e3fb5d7d43a999d99d07684f611575
f-kubotar <f.kubotar@paperboy.co.jp>
2014-06-18 11:18:17 +0900

    add jpeg-archie stream-read version

--- Library/Formula/jpeg-archive.rb
+++ Library/Formula/jpeg-archive.rb
@@ -2,14 +2,18 @@ require 'formula'
 
 class JpegArchive < Formula
   homepage 'https://github.com/danielgtaylor/jpeg-archive'
-  url 'https://github.com/danielgtaylor/jpeg-archive/archive/1.0.1.tar.gz'
+  url 'https://github.com/f-kubotar/jpeg-archive/archive/1.0.1s.tar.gz'
+  version '1.0.1'
   sha1 '45d36e74ae795d25e8cbf20d2ccd3ae17d4c6f7b'
 
   depends_on 'jpeg-turbo'
 
   def install
     system "make"
-    system "make install"
+    mkdir_p "#{prefix}/bin"
+    %w(jpeg-recompress jpeg-hash jpeg-compare).each do |bin_file|
+      cp bin_file, "#{prefix}/bin/#{bin_file}"
+    end
   end
 
   test do
